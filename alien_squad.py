cbce46f27f421c656672fe610b594c71ce187c2b
= <=>
2021-12-17 23:21:16 -0300

    Vertical squad move

--- alien_squad.py
+++ alien_squad.py
@@ -3,32 +3,40 @@ from gameObject import GameObject
 import alien
 
 class AlienSquad(GameObject):
-    x_limit = 54
+    x_limit = 60
     direction = 1
 
-    aliens = [[0 for i in range(8)] for j in range(5)]
+    rows = 8
+    aliens_per_rows = 10
+
+    aliens = [[0 for i in range(10)] for j in range(8)]
  
     def __init__(self, win, y, x):
         super().__init__(win, y, x)
         self.win = win
-        for yaxis in range(5):
-            for xaxis in range(8) :                  
-                self.aliens[yaxis][xaxis] = Alien(self.win, self.y + yaxis, self.x + (5 * xaxis))
+        for yaxis in range(self.rows):
+            for xaxis in range(self.aliens_per_rows) :                  
+                self.aliens[yaxis][xaxis] = Alien(self.win, self.y + yaxis, self.x + (3 * xaxis))
 
     def draw(self):
-#        for i in range(75):
-        for yaxis in range(5):
-            for xaxis in range(8) :                  
+        for yaxis in range(self.rows):
+            for xaxis in range(self.aliens_per_rows) :                  
                 self.aliens[yaxis][xaxis].draw()
+
+    def clearPreviousRow(self):
+        self.win.addstr(self.y-1, 2, 98 * " ") 
 
     def move(self):
         if self.x == self.x_limit or self.x == 2:
-            self.direction = self.direction * (-1)
+            self.direction = self.direction * (-1)            
+            self.y += 1
+            self.clearPreviousRow()
             
         self.x += self.direction
-        for yaxis in range(5):
-            for xaxis in range(8) :                  
+        for yaxis in range(self.rows):
+            for xaxis in range(self.aliens_per_rows) :                  
                 self.aliens[yaxis][xaxis].clear();
-                self.aliens[yaxis][xaxis].x = self.x + (5 * xaxis)
+                self.aliens[yaxis][xaxis].x = self.x + (3 * xaxis)
+                self.aliens[yaxis][xaxis].y = self.y + yaxis
 
                 
